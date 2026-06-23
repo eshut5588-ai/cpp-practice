e1865b37a3fbdec31927c40f12895eaa52ce1389
eshut5588-ai <eshut5588@gmail.com>
2026-06-23 21:27:17 +0530

    PrintNumber.cpp

---
+++ PrintNumber.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 5 , y = 7;
+    cout<<" Print The Number :- "<<endl;
+    cout<<" X = "<<x<<endl;
+    cout<<" Y = "<<y;
+    return 0;
+    
+}
