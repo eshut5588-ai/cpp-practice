a9ee1a14c9a1205d842ce27f15d31069278c337b
eshut5588-ai <eshut5588@gmail.com>
2026-06-25 20:53:28 +0530

    ArithmeticOperations.cpp

---
+++ ArithmeticOperations.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 6 , y = 8 ;
+    cout<<"Sum of Two number = "<<x+y<<endl;
+    int a = 9 , b = 5 ;
+    cout<<"Subtraction of Two Number =  "<<a-b<<endl;
+    float c = 5 , d = 2 ;
+    cout<<"Division of two Number = "<<c/d<<endl;
+    int e = 2 , f = 2 ;
+    cout<<"Multiplication of Two Number = "<<e*f<<endl;
+}
